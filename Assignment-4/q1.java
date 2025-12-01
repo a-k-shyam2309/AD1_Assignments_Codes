ebe29dd6b8617f743f551696dd42904b5e0ee945
Aditya Kumar Shyam <a.k.shyam2309@gmail.com>
2025-12-01 12:29:02 +0530

    Update q1.java

--- Assignment-4/q1.java
+++ Assignment-4/q1.java
@@ -1,10 +1,23 @@
 public class q1 {
     public static void main(String[] args) {
         int arr[] = {12,23,34,45,56};
-        for (int i = 0; i < arr.length-1; i++) {
-                if (arr[i] < arr[i+1]) {
-                    
-                }
+        int n = arr.length;
+        int asc = 0;
+        int des = 0;
+        for (int i = 0; i < n-1; i++) {
+            if (arr[i] < arr[i+1]){
+                asc++;
+            }
+            else{
+                des++;
+            }
+        }
+        if (asc == n-1){
+            System.out.println("Yes Ascending");
+        }else if (des == n-1){
+            System.out.println("Yes Descending");
+        }else{
+            System.out.println("No");
         }
     }
 }
