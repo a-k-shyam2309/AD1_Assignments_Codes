fb9dbb47618beda61c8352c2d10fccfceee0b875
Aditya Kumar Shyam <a.k.shyam2309@gmail.com>
2026-01-26 22:30:40 +0530

    Add triangle counting functionality in CountTriangles
    Implemented a method to count possible triangles from an array of integers.

--- Assignment-4/q5.java
+++ Assignment-4/q5.java
@@ -1,6 +1,31 @@
-public class q5 {
-    public static void main(String[] args) {
-        int arr[] = {1,2,3,4,5};
+import java.util.Arrays;
 
+public class CountTriangles {
+
+    public static int countTriangles(int[] arr) {
+        int n = arr.length;
+        int count = 0;
+
+        Arrays.sort(arr);
+
+        for (int k = n - 1; k >= 2; k--) {
+            int i = 0;
+            int j = k - 1;
+
+            while (i < j) {
+                if (arr[i] + arr[j] > arr[k]) {
+                    count += (j - i);
+                    j--;
+                } else {
+                    i++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println(countTriangles(arr));
     }
 }
