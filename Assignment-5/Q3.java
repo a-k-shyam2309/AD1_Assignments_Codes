15c47051888be76f77f1caf8ec4decb3c2f4c163
Aditya Kumar Shyam <a.k.shyam2309@gmail.com>
2026-01-26 23:00:17 +0530

    Update Q3.java

--- Assignment-5/Q3.java
+++ Assignment-5/Q3.java
@@ -1,32 +1,68 @@
-import java.util.PriorityQueue;
+import java.util.Scanner;
 
-public class Q3 {
+class KthLargestHeap {
 
-    // Function to find the kth largest element
-    public static int findKthLargest(int[] nums, int k) {
-        // Create a min-heap of size k
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>(k);
+    // Heapify function (Max Heap)
+    static void heapify(int[] arr, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
 
-        // Add the first k elements to the heap
-        for (int i = 0; i < k; i++) {
-            minHeap.add(nums[i]);
+        if (left < n && arr[left] > arr[largest])
+            largest = left;
+
+        if (right < n && arr[right] > arr[largest])
+            largest = right;
+
+        if (largest != i) {
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+
+            heapify(arr, n, largest);
         }
+    }
 
-        // Process the remaining elements
-        for (int i = k; i < nums.length; i++) {
-            if (nums[i] > minHeap.peek()) {
-                minHeap.poll();
-                minHeap.add(nums[i]);
-            }
+    // Build Max Heap
+    static void buildHeap(int[] arr, int n) {
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
         }
+    }
 
-        // The root of the heap is the kth largest element
-        return minHeap.peek();
+    // Delete max element from heap
+    static int deleteMax(int[] arr, int n) {
+        int max = arr[0];
+        arr[0] = arr[n - 1];
+        heapify(arr, n - 1, 0);
+        return max;
     }
 
     public static void main(String[] args) {
-        int[] nums = {3, 2, 1, 5, 6, 4};
-        int k = 2;
-        System.out.println("The " + k + "th largest element is: " + findKthLargest(nums, k));
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter k: ");
+        int k = sc.nextInt();
+
+        buildHeap(arr, n);
+
+        int result = 0;
+        int size = n;
+
+        for (int i = 1; i <= k; i++) {
+            result = deleteMax(arr, size);
+            size--;
+        }
+
+        System.out.println("Kth largest element is: " + result);
     }
 }
